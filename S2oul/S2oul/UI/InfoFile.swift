5779c22935bddd75f7a6ad89f830a4800734989e
이현욱 <hjhsha827@gmail.com>
2019-09-19 22:31:51 +0900

    CREATE Shape
    Info 파트, shape

---
+++ S2oul/S2oul/UI/InfoFile.swift
@@ -0,0 +1,14 @@
+//
+//  InfoFile.swift
+//  S2oul
+//
+//  Created by 이현욱 on 19/09/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import Foundation
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+}
---
+++ S2oul/S2oul/UI/InfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  InfoVC.swift
+//  
+//
+//  Created by 이현욱 on 19/09/2019.
+//
+
+import UIKit
+import Foundation
+
+
+class InfoVC: UIViewController {
+    
+    @IBOutlet weak var showBtn: UIButton!
+    @IBOutlet weak var theaterBtn: UIButton!
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+
+    
+
+
